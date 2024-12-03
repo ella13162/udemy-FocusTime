d20de32da4147d00b8684eb4548685135eb9acb1
Elzbieta Strzyz <elzbietastrzyz@Elzbietas-MacBook-Pro.local>
2024-12-03 21:33:11 +0000

    starting app

---
+++ app.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS ==='android' ? StatusBar.currentHeight:0,
+    backgroundColor: '#252250',
+  },
+});
